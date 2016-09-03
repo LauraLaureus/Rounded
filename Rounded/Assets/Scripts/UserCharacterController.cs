33242e9463bcf4f3f5b8b4a404e0a98ff8c41f04
LauraLaureus <l4depende@gmail.com>
2016-09-03 12:42:19 +0100

    Change state from scripting

--- Rounded/Assets/Scripts/UserCharacterController.cs
+++ Rounded/Assets/Scripts/UserCharacterController.cs
@@ -5,13 +5,20 @@ public class UserCharacterController : MonoBehaviour {
 
 	public float jumpAdjustement;
 	public float forwardAdjustement;
+
 	private Rigidbody rb;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		rb = gameObject.GetComponent<Rigidbody> ();
+		animator = gameObject.GetComponent<Animator> ();
 	}
-	
+
+	void Update(){
+		animator.SetFloat ("ForwardAdjustement", forwardAdjustement);
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		Vector3 movement = Vector3.forward*forwardAdjustement;
