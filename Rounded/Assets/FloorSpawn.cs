7bae5fede7faf4eea5855e62b8d0211a3b053930
LauraLaureus <l4depende@gmail.com>
2016-09-04 11:51:20 +0100

    Spawn the floor

---
+++ Rounded/Assets/FloorSpawn.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorSpawn : MonoBehaviour {
+
+	public GameObject floorPrefab;
+
+	void FixedUpdate () {
+		Instantiate (floorPrefab,this.transform.position,Quaternion.identity);
+	}
+}
---
+++ Rounded/Assets/Scripts/FactoryMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FactoryMovement : MonoBehaviour {
+
+	public float forwardAdjustement;
+
+	private Rigidbody rb;
+
+	void Start(){
+		rb = this.gameObject.GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Debug.DrawRay (this.transform.position, Vector3.up, Color.red);
+		Vector3 movement = Vector3.forward*forwardAdjustement;
+		rb.velocity += movement;
+	}
+}
