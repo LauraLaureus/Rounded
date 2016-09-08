8b87eccd635c127c5cff6a9cf1e2568efcfcb7c4
LauraLaureus <l4depende@gmail.com>
2016-09-08 16:40:54 +0100

    Destroy invisible background

--- Rounded/Assets/DisapearAndDestroy.cs
+++ Rounded/Assets/DisapearAndDestroy.cs
@@ -5,8 +5,12 @@ public class DisapearAndDestroy : MonoBehaviour {
 
 	public GameObject enemy;
 
+	void Start(){
+		enemy = GameObject.FindGameObjectWithTag ("Enemy");
+	}
+
 	void LateUpdate(){
-		if (Vector3.Distance (transform.position, enemy.transform.position) > 38f)
+		if (Vector3.Distance (transform.position, enemy.transform.position) > 38f && enemy.transform.position.z > transform.position.z)
 			Destroy (gameObject);
 	}
 }
